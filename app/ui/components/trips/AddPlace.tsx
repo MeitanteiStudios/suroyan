19532f0888ffbfb870033e46617233b067b9e2f1
IzatheGreat15 <20100869@usc.edu.ph>
2026-09-13 19:03:38 +0800

    Close modal on cancel button

--- app/ui/components/trips/AddPlace.tsx
+++ app/ui/components/trips/AddPlace.tsx
@@ -22,7 +22,7 @@ export default function AddPlace() {
                 <PlaceFields />
                 <div className="flex items-center justify-center gap-4 mt-16">
                     <input type="submit" value="Save" className="bg-sky-500 hover:bg-sky-400 cursor-pointer text-white font-medium px-6 py-2 rounded" />
-                    <button type="button" className="bg-gray-50 hover:bg-gray-100 border border-gray-200 text-sky-500 font-medium px-4 py-2 rounded">Cancel</button>
+                    <button type="button" onClick={() => setIsModalOpen(false)} className="bg-gray-50 hover:bg-gray-100 border border-gray-200 text-sky-500 font-medium px-4 py-2 rounded">Cancel</button>
                 </div>
             </Modal>
         </div>
